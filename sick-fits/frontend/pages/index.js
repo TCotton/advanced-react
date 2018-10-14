2c24b63277985e4787a478c28c56832c990d23ae
Andy Walpole <me@andywalpole.me>
2018-10-14 10:36:16 +0100

    first commit again

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react'
+
+const Home = props => (
+	<div>
+		<Link href="/sell">
+			<a>Sell</a>
+		</Link>
+	</div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react'
+
+const Sell = props => (
+	<div>
+		<Link href="/sell">
+			<a>Sell!</a>
+		</Link>
+	</div>
+);
+
+export default Sell;
